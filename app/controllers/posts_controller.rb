ac54df4c6ff6e779d6ca7239a838584f8fb47b22
Hunt Norment <hunt@sherpa.red>
2016-09-16 10:09:38 -0500

    added responders gem for rails 5

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,15 @@
 class PostsController < ApplicationController
+
+  def index
+    #respond_with Post.all
+  end
+
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:link, :title)
+  end
+
 end
